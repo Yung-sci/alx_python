dd31704543227e427798ffa912eeb2dbfc52705e
Yung-sci <yungsci101@gmail.com>
2023-10-03 15:09:54 +0100

    Conv

--- python-functions/2-temperature.py
+++ python-functions/2-temperature.py
@@ -1,3 +1,5 @@
+#fahrenheit to celsius
+#convert to censuis and strip
 def convert_to_celsius(fahrenheit):
     celsius = (5/9) * (fahrenheit - 32)
-    return round(celsius, 15)
+    return celsius
