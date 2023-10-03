6d6b4a8ae1c6f5d2f9846171d4632fd83a5fe386
Yung-sci <yungsci101@gmail.com>
2023-10-03 13:56:27 +0100

    Print_comb

--- python-hello_world/5-print_comb2.py
+++ python-hello_world/5-print_comb2.py
@@ -1,5 +1,5 @@
-for i in range(100):
-    if i < 99:
-        print(f"{i:02}, ", end="")
-    else:
-        print(f"{i:02}")
+for i in range(0, 100):
+  if i < 99:
+     print("{:02}, ".format(i), end = "" )
+  else :
+    print("{:02}".format(i))
