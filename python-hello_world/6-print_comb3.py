d08490f37f69468659c1492d415e52680352125c
Yung-sci <yungsci101@gmail.com>
2023-10-03 13:59:27 +0100

    Print_comb3

--- python-hello_world/6-print_comb3.py
+++ python-hello_world/6-print_comb3.py
@@ -1,6 +1,6 @@
 for i in range(10):
     for j in range(i + 1, 10):
-        if i < 9:
-            print(f"{i}{j:02}, ", end="")
-        else:
-            print(f"{i}{j:02}")
+        if i < 8:
+         print("{}{}, ".format(i, j), end = '')
+        else :
+           print("{}{}".format(i, j))
