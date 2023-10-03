5d5ee0c0e73e1a82bddcf3d8040b49f33c44b928
Yung-sci <yungsci101@gmail.com>
2023-10-03 23:45:16 +0100

    Firstmodule

---
+++ python-import_modules/0-add.py
@@ -0,0 +1,12 @@
+# Define a and b variables
+a = 1
+b = 2
+
+# Import the add function from add_0.py
+from add_0 import add
+
+# Calculate the result using the imported function
+result = add(a, b)
+
+# Print the result using string formatting
+print("{} + {} = {}".format(a, b, result))
