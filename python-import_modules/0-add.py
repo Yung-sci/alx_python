f1555e2565464b834d029cb30ac0216c3300ced9
Yung-sci <yungsci101@gmail.com>
2023-10-05 05:28:14 +0100

    Module

--- python-import_modules/0-add.py
+++ python-import_modules/0-add.py
@@ -1,12 +1,6 @@
-# Define a and b variables
+from add_0 import add 
 a = 1
 b = 2
-
-# Import the add function from add_0.py
-from add_0 import add
-
-# Calculate the result using the imported function
-result = add(a, b)
-
-# Print the result using string formatting
-print("{} + {} = {}".format(a, b, result))
+if __name__ == "__main__":
+    result = add(a,b)
+    print("{} +{} = {}".format(a,b,result))
