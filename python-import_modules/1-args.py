4e0c47b1ea5d966059823eee1b40e011eb5e0522
Yung-sci <yungsci101@gmail.com>
2023-10-05 08:25:23 +0100

    Arguments

---
+++ python-import_modules/1-args.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+import sys
+
+if __name__ == "__main__":
+    arguments = sys.argv[1:]
+    num_arguments = len(arguments)
+
+    if num_arguments == 0:
+        print("0 arguments.")
+    elif num_arguments == 1:
+        print("1 arguments:")
+    else:
+        print(f"{num_arguments} arguments")
+
+    if num_arguments > 0:
+        for i, arg in enumerate(arguments, start=1):
+            print(f"{i}: {arg}")
