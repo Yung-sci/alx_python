03b4d4792b7c80dcca1d9645e3604dccb5f2ae16
Yung-sci <yungsci101@gmail.com>
2023-10-20 21:38:27 +0100

    hmmn

--- python-data_structures/0-no_c.py
+++ python-data_structures/0-no_c.py
@@ -5,7 +5,3 @@ def no_c(my_string):
             contain.append(char)
     result = ''.join(contain)
     return result
-
-print(no_c("Holberton School"))
-print(no_c("Chicago"))
-print(no_c("C is fun!"))
