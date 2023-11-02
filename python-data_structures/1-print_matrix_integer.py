759178fb7521ff76b7d8c83d4de4e025e63de1bc
Yung-sci <yungsci101@gmail.com>
2023-11-01 18:52:30 -0700

    Print_Matrix

---
+++ python-data_structures/1-print_matrix_integer.py
@@ -0,0 +1,9 @@
+def print_matrix_integer(matrix=[[]]):
+    matrix = [
+    [1, 2, 3],
+    [5, 6, 9],
+    [4, 0, 7]
+    ]
+print_matrix_integer(str.format("matrix"))
+print(str.format("matrix"))
+print_matrix_integer()
