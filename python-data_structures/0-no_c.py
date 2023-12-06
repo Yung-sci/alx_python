354ab7df86294dd57ffcd32b945f0988db33e5fa
Yung-sci <yungsci101@gmail.com>
2023-12-06 16:28:17 +0100

    hh

--- python-data_structures/0-no_c.py
+++ python-data_structures/0-no_c.py
@@ -1,4 +1,3 @@
-'''
 def no_c(my_string):
     contain = []
     for char in my_string:
@@ -6,28 +5,3 @@ def no_c(my_string):
             contain.append(char)
     result = ''.join(contain)
     return result
-'''
-'''
-def no_c(my_string):
-    for char in my_string:
-        if char == 'c' or char == 'C':
-            my_string.pop
-    result = no_c
-    return result
-
-print(no_c("Holberton School"))
-print(no_c("Chicago"))
-print(no_c("C is fun!"))
-'''
-
-
-def no_c(my_string):
-    result = ""
-    for char in my_string:
-        if char.lower() != 'c':
-            result += char
-    return result
-
-print(no_c("Holberton School"))
-print(no_c("Chicago"))
-print(no_c("C is fun!"))
