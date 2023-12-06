0ad2699c1d4e2cf0a240c5748fdebd25c9237198
Yung-sci <yungsci101@gmail.com>
2023-12-06 16:51:46 +0100

    hh

---
+++ python-data_structures/calculator.py
@@ -0,0 +1,8 @@
+# prompt command
+input("Enter first digit?")
+input("Enter second digit")
+# calculation
+sum=int{first digit}+{second digit}
+print(sum)
+difference=int{first digit}-{secon digit}
+print(difference)
